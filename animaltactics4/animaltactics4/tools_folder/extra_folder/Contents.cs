2735dbb502874c77afa3c7a9b0ddbc81696748de
nbarray <Nbarray@gmail.com>
2012-06-14 19:03:52 +0000

    IronPenguin : Resolution !!

--- animaltactics4/animaltactics4/tools_folder/extra_folder/Contents.cs
+++ animaltactics4/animaltactics4/tools_folder/extra_folder/Contents.cs
@@ -13,10 +13,12 @@ namespace animaltactics4
         static public Dictionary<string, Texture2D> textures = new Dictionary<string,Texture2D>();
         static public Dictionary<string, SpriteFont> fonts = new Dictionary<string,SpriteFont>();
         static private SpriteBatch Atsushi_Okhubo;
+        static private float screenWidth, screenHeight, pprc;
 
         static public void Initialize(GraphicsDevice device_)
         {
             Atsushi_Okhubo = new SpriteBatch(device_);
+            adapter(device_.DisplayMode.Width, device_.DisplayMode.Height);
         }
 
         static public void LoadContent(ContentManager content_)
@@ -29,16 +31,32 @@ namespace animaltactics4
             fonts.Add("bouton", content_.Load<SpriteFont>("SPriteFont\\sfBouton"));
         }
 
+        static public void adapter(float screenWidth_, float screenHeight_)
+        {
+            screenHeight = screenHeight_;
+            screenWidth = screenWidth_;
+            pprc = Math.Min(screenWidth / Divers.X, screenHeight / Divers.Y);
+        }
+
         static public void Draw(string name_, Rectangle rect_)
         {
             Atsushi_Okhubo.Begin();
-            Atsushi_Okhubo.Draw(textures[name_], rect_, Color.White);
+            Atsushi_Okhubo.Draw(textures[name_],
+                new Rectangle(rect_.X + (int)((screenWidth - Divers.X * pprc) / 2),
+                    rect_.Y + (int)((screenHeight - Divers.Y * pprc) / 2),
+                    (int)(rect_.Width * pprc), (int)(rect_.Height * pprc)), Color.White);
             Atsushi_Okhubo.End();
         }
         static public void DrawStringInBox(string text_, Rectangle rect_)
         {
             Atsushi_Okhubo.Begin();
-            Atsushi_Okhubo.DrawString(fonts["bouton"], text_, new Vector2(rect_.X + (rect_.Width / 2) - (fonts["bouton"].MeasureString(text_).X / 2), rect_.Y + (rect_.Height / 2) - (fonts["bouton"].MeasureString(text_).Y / 2)), Color.White);
+            Atsushi_Okhubo.DrawString(fonts["bouton"], text_,
+                new Vector2(rect_.X + (int)((screenWidth - Divers.X * pprc) / 2)
+                    + (int)(rect_.Width * pprc / 2)
+                    - (fonts["bouton"].MeasureString(text_).X / 2),
+                    rect_.Y + (int)((screenHeight - Divers.Y * pprc) / 2)
+                    + (int)(rect_.Height * pprc / 2) - (fonts["bouton"].MeasureString(text_).Y / 2)),
+                    Color.Pink);
             Atsushi_Okhubo.End();
         }
     }
