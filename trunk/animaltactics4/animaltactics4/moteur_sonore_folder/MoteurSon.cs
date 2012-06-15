37966c06f14ea6e99bcc8e6a0f742d1696696ecf
Nbarray@gmail.com <Nbarray@gmail.com@cf1efcda-0adc-3128-95ef-d8ac1c84941e>
2012-06-15 05:53:13 +0000

    Coldman: MoteurSon.cs + debut TextBox.cs

---
+++ trunk/animaltactics4/animaltactics4/moteur_sonore_folder/MoteurSon.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Media;
+
+namespace animaltactics4
+{
+    static class MoteurSon
+    {
+        static public Dictionary<string, Song> bankSong;
+        static public Dictionary<string, SoundEffect> bankEffect;
+
+        static public void Initialize(ContentManager content_)
+        {
+            MediaPlayer.IsRepeating = false;
+            bankSong = new Dictionary<string, Song>();
+            bankSong.Add("porte", content_.Load<Song>("Son\\Bruitage\\son_porte"));
+            bankSong.Add("bouton", content_.Load<Song>("Son\\Bruitage\\son_bouton"));
+
+            bankEffect = new Dictionary<string, SoundEffect>();
+            bankEffect.Add("deprimePorte", content_.Load<SoundEffect>("Son\\Bruitage\\depriemPorte"));
+        }
+
+        static public void Play(string name_)
+        {
+            MediaPlayer.Play(bankSong[name_]);
+        }
+        static public void PlayFX(string name_)
+        {
+            bankEffect[name_].CreateInstance().Play();
+        }
+    }
+}
---
+++ trunk/animaltactics4/animaltactics4/structure_du_jeu_folder/text_folder/TextBox.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace animaltactics4
+{
+    class TextBox
+    {
+        public List<string> text;
+        public Rectangle rect;
+
+        public TextBox(Rectangle rect_)
+        {
+            rect = rect_;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw()
+        {
+            Contents.Draw("textbox", rect);
+            Contents.DrawStringInBoxJustify(text, rect);
+        }
+    }
+}
