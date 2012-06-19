e34c32d188cc23ba117c30c550ef1415eed96990
Nbarray@gmail.com <Nbarray@gmail.com@cf1efcda-0adc-3128-95ef-d8ac1c84941e>
2012-06-19 10:07:53 +0000

    Coldman : tes putain d'images

---
+++ trunk/animaltactics4/animaltactics4/moteur_reseau_folder/Server.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace animaltactics4.moteur_reseau_folder
+{
+    class Server
+    {
+        List<Socket> clients;
+
+        public Client()
+        {
+
+        }
+    }
+}
