bd915ccb6ebffd96115ecf03134c19de4f8bddf3
nbarray <Nbarray@gmail.com>
2012-06-19 10:07:53 +0000

    Coldman : tes putain d'images

---
+++ animaltactics4/animaltactics4/moteur_reseau_folder/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+
+namespace animaltactics4
+{
+    class Client
+    {
+        List<Socket> clients;
+
+        public Client()
+        {
+
+        }
+    }
+}
