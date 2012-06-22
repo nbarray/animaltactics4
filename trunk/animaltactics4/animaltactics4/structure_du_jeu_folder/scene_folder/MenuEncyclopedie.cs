403e0dbf5ff21ac171ffee0459b24943b5d36420
nbarray@gmail.com <nbarray@gmail.com@cf1efcda-0adc-3128-95ef-d8ac1c84941e>
2012-06-22 02:26:52 +0000

    IronPenguin

--- trunk/animaltactics4/animaltactics4/structure_du_jeu_folder/scene_folder/MenuEncyclopedie.cs
+++ trunk/animaltactics4/animaltactics4/structure_du_jeu_folder/scene_folder/MenuEncyclopedie.cs
@@ -16,11 +16,11 @@ namespace animaltactics4
             textbox = new TextBox(new Rectangle(300, 100, 800, Divers.Y - 300));
             boutons.Add(new BoutonLien(Divers.X / 2 - 200, 700, new Rectangle(0, 0, 800, 300), null, 5));
             onglets = new Onglet(new Rectangle(15, 100, 285, Divers.Y - 300));
-            onglets.Add(Dico.langues[Dico.current][52]);
-            onglets.Add(Dico.langues[Dico.current][53]);
-            onglets.Add(Dico.langues[Dico.current][54]);
-            onglets.Add(Dico.langues[Dico.current][55]);
-            onglets.Add(Dico.langues[Dico.current][56]);
+            onglets.Add(52);
+            onglets.Add(53);
+            onglets.Add(54);
+            onglets.Add(55);
+            onglets.Add(56);
         }
 
         public override void UpdateScene(GameTime gameTime)
